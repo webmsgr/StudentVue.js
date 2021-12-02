4206832cd6555d6c5741c05a52b7c818382abc96
Restyled.io <commits@restyled.io>
2021-12-02 19:32:02 +0000

    Restyled by clang-format

--- doTests.js
+++ doTests.js
@@ -1,4 +1,5 @@
-/** This file does automated tests on the studentvue api
+/**
+ * This file does automated tests on the studentvue api
  * making sure what the api returns matches the types written
  */
 let StudentVue = require("./dist/index.js")
