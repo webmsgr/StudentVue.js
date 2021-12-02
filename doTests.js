4a8e2bbfd8d42d8a40cf06b2b9cc5145761262fc
Restyled.io <commits@restyled.io>
2021-12-02 19:32:06 +0000

    Restyled by prettier

--- doTests.js
+++ doTests.js
@@ -2,4 +2,4 @@
  * This file does automated tests on the studentvue api
  * making sure what the api returns matches the types written
  */
-let StudentVue = require("./dist/index.js")
+let StudentVue = require("./dist/index.js");
